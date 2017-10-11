b260d2b7a87cfce5f9141f8b66d13517946c9bc2
Ricardo Olivieri <roliv@us.ibm.com>
2017-10-11 16:00:17 -0500

    improving error handling

--- Sources/KituraBuddy/PersistableExtension.swift
+++ Sources/KituraBuddy/PersistableExtension.swift
@@ -27,33 +27,60 @@ extension Persistable {
     // create
     static func create(model: Model, respondWith: @escaping (Model?, Error?) -> Void) {
         client.post(route, data: model) { (model: Model?, error: Error?) -> Void in
-            if let model = model {
-                respondWith(model, nil)
-            } else {
+            // First determine if error was not nil
+            if error != nil {
                 respondWith(nil, error)
+                return
             }
+
+            // Next, determine if model was nil
+            guard let model = model else {
+                // If we get here, then error and model were both nil
+                // Then we should create our own custom error instance
+                // and send it to the user of our library
+                //TODO: create error instance
+                respondWith(nil, error)
+                return
+            }
+
+            // If we get here, then model was not nil and error was nil
+            respondWith(model, nil)
         }
     }
     
     // read
     static func read(id: Id, respondWith: @escaping (Model?, Error?) -> Void) {
         client.get(route, identifier: id) { (model: Model?, error: Error?) -> Void in
-            if let model = model {
-                respondWith(model, nil)
-            } else {
+            if error != nil {
                 respondWith(nil, error)
+                return
             }
+
+            guard let model = model else {
+                //TODO: create error instance
+                respondWith(nil, error)
+                return
+            }
+            
+            respondWith(model, nil)
         }
     }
     
     // read all
     static func read(respondWith: @escaping ([Model]?, Error?) -> Void) {
         client.get(route) { (model: [Model]?, error: Error?) -> Void in
-            if let model = model {
-                respondWith(model, nil)
-            } else {
+            if error != nil {
                 respondWith(nil, error)
+                return
             }
+
+            guard let model = model else {
+                //TODO: create error instance
+                respondWith(nil, error)
+                return
+            }
+            
+            respondWith(model, nil)
         }
     }
 
@@ -61,11 +88,18 @@ extension Persistable {
     // update
     static func update(id: Id, model: Model, respondWith: @escaping (Model?, Error?) -> Void) {
         client.put(route, identifier: id, data: model) { (model: Model?, error: Error?) -> Void in
-            if let model = model {
-                respondWith(model, nil)
-            } else {
+            if error != nil {
                 respondWith(nil, error)
+                return
             }
+
+            guard let model = model else {
+                //TODO: create error instance
+                respondWith(nil, error)
+                return
+            }
+            
+            respondWith(model, nil)
         }
     }
 
@@ -73,6 +107,11 @@ extension Persistable {
     static func delete(id: Id, respondWith: @escaping (Error?) -> Void) {
         // Perform delete REST call...
         client.delete(route, identifier: id) { (error: Error?) -> Void in
+            guard let _ = error else {
+                //TODO: create error instance
+                respondWith(error)
+                return
+            }
             respondWith(error)
         }
     }
@@ -81,6 +120,11 @@ extension Persistable {
     static func delete(respondWith: @escaping (Error?) -> Void) {
         // Perform delete REST call...
         client.delete("/") { (error: Error?) -> Void in
+            guard let _ = error else {
+                //TODO: create error instance
+                respondWith(error)
+                return
+            }
             respondWith(error)
         }
     }
