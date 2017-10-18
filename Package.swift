11225935b4212673ad9bfb6d8f805428be36ecb9
seabaylea <baileyc@uk.ibm.com>
2017-10-18 14:54:38 +0100

    Export KituraBuddy for use in Swift PM applications

--- Package.swift
+++ Package.swift
@@ -21,6 +21,12 @@ import PackageDescription
 
 let package = Package(
     name: "KituraBuddy",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "KituraBuddy",
+            targets: ["KituraBuddy"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/IBM-Swift/SwiftyRequest.git", .upToNextMinor(from: "0.0.5")),
