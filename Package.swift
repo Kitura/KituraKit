4956040e69f5bdfed597c3004babfbf86697f4d3
Ricardo Olivieri <roliv@us.ibm.com>
2017-10-25 17:07:08 -0500

    Added products entry to package.swift

--- Package.swift
+++ Package.swift
@@ -21,6 +21,13 @@ import PackageDescription
 
 let package = Package(
     name: "KituraKit",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "KituraKit",
+            targets: ["KituraKit"]
+        ),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/IBM-Swift/SwiftyRequest.git", .upToNextMinor(from: "0.0.5")),
