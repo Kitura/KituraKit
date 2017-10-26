0dd014192a38fc7b52536e04a52746d8d2ae5974
ShihabMehboob <shihab.mehboob1@ibm.com>
2017-10-26 12:02:39 +0100

    Further documentation

--- Sources/KituraKit/RequestErrorExtension.swift
+++ Sources/KituraKit/RequestErrorExtension.swift
@@ -18,24 +18,48 @@ import Foundation
 import KituraContracts
 import SwiftyRequest
 
+/// An extension to Kitura RequestErrors with additional error codes specifically for the client.
 extension RequestError {
 
+    /// An initializer to set up the client error codes.
+    /// - Parameter clientErrorCode: The custom error code for the client.
     init(clientErrorCode: Int) {
         self.init(rawValue: clientErrorCode)
     }
 
+    /// An HTTP 600 unknown error
     public static var clientErrorUnknown = RequestError(clientErrorCode: 600)
+    
+    /// An HTTP 601 connection error
     public static var clientConnectionError = RequestError(clientErrorCode: 601)
+    
+    /// An HTTP 602 no data error
     public static var clientNoData = RequestError(clientErrorCode: 602)
+    
+    /// An HTTP 603 serialization error
     public static var clientSerializationError = RequestError(clientErrorCode: 603)
+    
+    /// An HTTP 604 deserialization error
     public static var clientDeserializationError = RequestError(clientErrorCode: 604)
+    
+    /// An HTTP 605 encoding error
     public static var clientEncodingError = RequestError(clientErrorCode: 605)
+    
+    /// An HTTP 606 file manager error
     public static var clientFileManagerError = RequestError(clientErrorCode: 606)
+    
+    /// An HTTP 607 invalid file error
     public static var clientInvalidFile = RequestError(clientErrorCode: 607)
+    
+    /// An HTTP 608 invalid substitution error
     public static var clientInvalidSubstitution = RequestError(clientErrorCode: 608)
 }
 
+/// An extension to Kitura RequestErrors with additional error codes specifically for the client.
 extension RequestError {
+    
+    /// An initializer to switch between different error types.
+    /// - Parameter restError: The custom error type for the client.
     init(restError: RestError) {
         switch restError {
         case .erroredResponseStatus(let code): self = RequestError(httpCode: code)
