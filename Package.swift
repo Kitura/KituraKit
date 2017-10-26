9307164a8829b0c395cb379eed672e10ea2717a2
Ricardo Olivieri <roliv@us.ibm.com>
2017-10-26 15:16:00 -0500

    updated package.swift (which we will need to do again once we release kitura 2.0)

--- Package.swift
+++ Package.swift
@@ -32,7 +32,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/IBM-Swift/SwiftyRequest.git", .upToNextMinor(from: "0.0.5")),
         .package(url: "https://github.com/IBM-Swift/Kitura.git", .branch("issue.swift4")),
-        .package(url: "https://github.com/IBM-Swift/KituraContracts.git", .branch("master"))
+        .package(url: "https://github.com/IBM-Swift/KituraContracts.git", .upToNextMinor(from: "0.0.11"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
