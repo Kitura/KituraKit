6ce2f0af0561c0d87cafddbc4b4bc6045e927e43
David Dunn <davdunn2@uk.ibm.com>
2017-11-15 16:08:20 +0000

    Enable automatic tagging of master

--- ci/release.sh
+++ ci/release.sh
@@ -77,3 +77,22 @@ rm -rf ../../../swift-4.0-RELEASE-ubuntu14.04/
 git add -A
 git commit -m "Updating pod branch to latest version"
 git push origin pod
+
+git checkout master
+
+if [ -f VERSION ]; then
+    BASE_VERSION_STRING='cat VERSION'
+    BASE_VERSION_LIST=(`echo $BASE_VERSION_STRING | tr '.' ' '`)
+    V_MAJOR=${BASE_VERSION_LIST[0]}
+    V_MINOR=${BASE_VERSION_LIST[1]}
+    V_PATCH=${BASE_VERSION_LIST[2]}
+
+    V_PATCH=$((V_PATCH + 1))
+    NEW_VERSION="$V_MAJOR.$V_MINOR.$V_PATCH"
+    
+    echo $NEW_VERSION > VERSION
+    git add VERSION
+    git commit -m "New release of KituraKit at $NEW_VERSION"
+    git tag -a -m "Tagging version $NEW_VERSION" "v$NEW_VERSION"
+    git push origin --tags
+fi
