a1c9bc7735e287203ba89f91245305c3451cd200
David Dunn <davdunn2@uk.ibm.com>
2017-11-15 17:48:28 +0000

    Adds v to version

--- ci/update.sh
+++ ci/update.sh
@@ -96,5 +96,5 @@ if [ -f VERSION ]; then
     git add VERSION
     git commit -m "New release of KituraKit at $NEW_VERSION"
     git tag -a -m "Tagging version $NEW_VERSION" "v$NEW_VERSION"
-    git push origin $NEW_VERSION
+    git push origin v$NEW_VERSION
 fi
