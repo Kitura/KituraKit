23da38981289d6f8815c6a7375a34feb4f5a6e0c
Shihab-Mehboob1 <shihab.mehboob1@ibm.com>
2018-04-10 12:19:25 +0100

    Additions to helpful error messages

--- Sources/KituraKit/RequestErrorExtension.swift
+++ Sources/KituraKit/RequestErrorExtension.swift
@@ -31,28 +31,28 @@ public extension RequestError {
     public static var clientErrorUnknown = RequestError(clientErrorCode: 600, clientErrorDescription: "An unknown error occurred")
     
     /// An HTTP 601 connection error
-    public static var clientConnectionError = RequestError(clientErrorCode: 601, clientErrorDescription: "A connection error occurred, cannot connect to the server")
+    public static var clientConnectionError = RequestError(clientErrorCode: 601, clientErrorDescription: "A connection error occurred, cannot connect to the server. Please ensure that the server is started and running, with the correct port and URL ('ToDoServer' if using the sample app).")
     
     /// An HTTP 602 no data error
-    public static var clientNoData = RequestError(clientErrorCode: 602, clientErrorDescription: "A no data error occurred")
+    public static var clientNoData = RequestError(clientErrorCode: 602, clientErrorDescription: "A no data error occurred. Please ensure that data exists, and is being passed to the correct destination.")
     
     /// An HTTP 603 serialization error
-    public static var clientSerializationError = RequestError(clientErrorCode: 603, clientErrorDescription: "A serialization error occurred")
+    public static var clientSerializationError = RequestError(clientErrorCode: 603, clientErrorDescription: "A serialization error occurred. Please ensure that the type of the data being serialized is correct.")
     
     /// An HTTP 604 deserialization error
-    public static var clientDeserializationError = RequestError(clientErrorCode: 604, clientErrorDescription: "A deserialization error occurred")
+    public static var clientDeserializationError = RequestError(clientErrorCode: 604, clientErrorDescription: "A deserialization error occurred. Please ensure that the type of the data being deserialized is correct.")
     
     /// An HTTP 605 encoding error
-    public static var clientEncodingError = RequestError(clientErrorCode: 605, clientErrorDescription: "An encoding error occurred")
+    public static var clientEncodingError = RequestError(clientErrorCode: 605, clientErrorDescription: "An encoding error occurred. Please ensure that the types and format of the data being passed is correct.")
     
     /// An HTTP 606 file manager error
-    public static var clientFileManagerError = RequestError(clientErrorCode: 606, clientErrorDescription: "A file manager error occurred")
+    public static var clientFileManagerError = RequestError(clientErrorCode: 606, clientErrorDescription: "A file manager error occurred. Please ensure that the file exists, and correct permissions to the file manager are present for the user.")
     
     /// An HTTP 607 invalid file error
-    public static var clientInvalidFile = RequestError(clientErrorCode: 607, clientErrorDescription: "An invalid file error occurred")
+    public static var clientInvalidFile = RequestError(clientErrorCode: 607, clientErrorDescription: "An invalid file error occurred. Please ensure that the file type and contents are correct.")
     
     /// An HTTP 608 invalid substitution error
-    public static var clientInvalidSubstitution = RequestError(clientErrorCode: 608, clientErrorDescription: "An invalid substitution error occurred")
+    public static var clientInvalidSubstitution = RequestError(clientErrorCode: 608, clientErrorDescription: "An invalid substitution error occurred. Please ensure that the data being substituted is correct.")
 }
 
 /// An extension to Kitura RequestErrors with additional error codes specifically for the client.
